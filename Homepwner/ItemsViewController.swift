d4d958137eb9ff6bac8f117a20f87f2b9c7cfc96
cpoles <carlos.poles@griffithuni.edu.au>
2017-11-26 23:13:04 +1000

    TableView displaying random values from itemStore

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -13,26 +13,11 @@ class ItemsViewController: UITableViewController {
     var itemStore: ItemStore!
     
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        // only one section in this tableView
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -44,7 +29,7 @@ class ItemsViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
 
         // Configure the cell...
         // Set the text on the cell with the description of the item that is at the nth index of items
