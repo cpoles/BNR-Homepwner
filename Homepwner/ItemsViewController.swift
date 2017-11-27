5be109052717e1a1acad932bbdf2cf8e2b916953
cpoles <carlos.poles@griffithuni.edu.au>
2017-11-27 14:06:13 +1000

    Defined contentInset and scrollIndicatorInsets for the TableView.

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -12,7 +12,21 @@ class ItemsViewController: UITableViewController {
     
     var itemStore: ItemStore!
     
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Get the height of the status bar
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    }
+    
+    
+    
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
